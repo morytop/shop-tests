21d89b54a5ea6428bf11335a58051862779fbfe5
Piotr Moryto <piotrmoryto@gmail.com>
2024-04-08 14:02:42 +0200

    Add test data for user credentials

---
+++ src/test-data/user-data.ts
@@ -0,0 +1,4 @@
+export const testUser1 = {
+  email: process.env.USER_EMAIL ?? '[NOT SET]',
+  password: process.env.USER_PASSWORD ?? '[NOT SET]',
+};
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,12 @@
 import { AccountPage } from '../src/pages/account-page';
 import { LoginPage } from '../src/pages/login-page';
+import { testUser1 } from '../src/test-data/user-data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @login', async ({ page }) => {
-    const email = 'customer@practicesoftwaretesting.com';
-    const password = 'welcome01';
+    const email = testUser1.email;
+    const password = testUser1.password;
     const loginPage = new LoginPage(page);
     const accountPage = new AccountPage(page);
 

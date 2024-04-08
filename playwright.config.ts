c8e6f57551eb6ff23f38e24a9dde6ecde8f7950e
Piotr Moryto <piotrmoryto@gmail.com>
2024-04-08 13:53:10 +0200

    Add dotenv setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  globalSetup: 'src/global-setup.ts',
   timeout: 60_000,
   expect: { timeout: 10_000 },
   fullyParallel: true,
@@ -12,7 +13,7 @@ export default defineConfig({
   workers: undefined,
   reporter: 'html',
   use: {
-    baseURL: 'https://practicesoftwaretesting.com/#',
+    baseURL: process.env.BASE_URL,
     actionTimeout: 0,
     trace: 'retain-on-failure',
     video: 'retain-on-failure',

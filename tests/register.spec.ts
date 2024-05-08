50c275bfb2e302a33ab69b51357a57afbe55b9e4
Piotr Moryto <piotrmoryto@gmail.com>
2024-05-08 14:24:08 +0200

    Add faker data

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,21 +1,28 @@
 import { AccountPage } from '../src/pages/account-page';
 import { LoginPage } from '../src/pages/login-page';
 import { RegisterPage } from '../src/pages/register-page';
+import { faker } from '@faker-js/faker';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify register @register', () => {
   test('register with correct data and login', async ({ page }) => {
-    const firstName = 'Joana';
-    const lastName = 'Smith';
-    const dateOfBirth = '1993-01-08';
-    const address = 'Test';
-    const postcode = '00-000';
-    const city = 'Test';
-    const state = 'test';
-    const country = 'AZ';
-    const phone = '000000000';
-    const email = `joana${new Date().getTime()}@test1.pl`;
-    const password = 'Tojestjoana1!';
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const dateOfBirth = faker.date
+      .birthdate({ min: 18, max: 65, mode: 'age' })
+      .toLocaleDateString('en-CA');
+    const address = faker.location.street();
+    const postcode = faker.location.zipCode();
+    const city = faker.location.city();
+    const state = faker.location.state();
+    const country = faker.location.countryCode();
+    const phone = faker.string.numeric(8);
+    const email = faker.internet.email();
+    const password = faker.internet.password({
+      length: 20,
+      pattern: /^[a-z ,.'-]+$/i,
+      prefix: '1!',
+    });
     const registerPage = new RegisterPage(page);
     const loginPage = new LoginPage(page);
     const accountPage = new AccountPage(page);
